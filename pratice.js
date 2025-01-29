cee40d1a1738f80271460514fe54d11ae32d3eb2
akinolablessing <akinolablessing1890@gmail.com>
2025-01-29 09:53:12 +0100

    first push

---
+++ pratice.js
@@ -0,0 +1,17 @@
+var cars =["benz","Gip","Toyota","Honda"]
+console.log(cars.length)
+console.log("======================================")
+for(i = 0; i < cars.length; i++){
+console.log(cars[i])
+	if(cars[i] == "benz"){
+ 	console.log("You are driving nice car: "+ cars[i])
+  }
+}
+console.log("======================================")
+let array = [5,10,15,20,25]
+let l = 0;
+while(l <array.length){
+console.log(array[l])
+l++;
+}
+
---
+++ var.js
@@ -0,0 +1,49 @@
+var var1 = 2;
+var var2 = 3;
+var var3 = 4;
+
+let num = var2;
+var2 = var3;
+var3 = var1;
+console.log(num)
+console.log(var2)
+console.log(var3)
+
+const person ={
+name: "Ayomide",
+age: 21,
+isBlack: false,
+married: null,
+sex: "female",
+hobbies: ["dancing", "eating", "gossiping"]
+}
+let items = [1, "Ada", true, null, ];
+console.log(person)
+let x = 5;
+let y = 10;
+let z = x+y;
+console.log(2*(z))
+
+let num1 = 20;
+let num2 = num1 % 10;
+console.log(num2 + num1)
+
+let t = 10;
+let n = 2;
+let v = t ** n
+console.log(v)
+
+let a = 6;
+let d = a + 10;
+let s = a - 10;
+let m = a % 10;
+let sum = d * s * m;
+let sum1 = sum/a;
+console.log(sum1)
+
+
+ 
+
+
+
+
